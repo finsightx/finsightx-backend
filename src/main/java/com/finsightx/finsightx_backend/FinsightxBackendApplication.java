f779fd5a0f5953fea94b022d30f84f5af9e7551d
YUNA <yuna83872@gmail.com>
2025-07-17 00:15:18 +0900

    Initial commit

---
+++ src/main/java/com/finsightx/finsightx_backend/FinsightxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.finsightx.finsightx_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinsightxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinsightxBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finsightx/finsightx_backend/FinsightxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finsightx.finsightx_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinsightxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
